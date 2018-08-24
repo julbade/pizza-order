18994886817d162e84261837cc45b1066cd9eb64
julius bade <julbade21@gmail.com>
2018-08-24 14:23:34 -0700

    it will alert if there is no input of toppings

--- pizza/js/scripts.js
+++ pizza/js/scripts.js
@@ -1,11 +1,12 @@
-// function Pizza(name, topping, cost) {
-//   this.name = name;
-//   this.toppings = toppings;
-//   this.cost = cost;
-// }
-// var newPizza = new Pizza("Pepperoni", ["pepperoni", "cheese"], 10 );
-//
-//
+function Pizza(name, topping, cost) {
+  this.name = name;
+  this.toppings = toppings;
+  this.cost = cost;
+}
+
+var newPizza = 0
+
+
 
 
 
@@ -43,10 +44,34 @@
       $(".firstname").text(firstnameInput);
       $(".lastname").text(lastnameInput);
       $(".address").text(addressInput);
-      $("#text2").show();
-  });
+      $("#frontpage").hide();
+      $("#nextpage").show();
+    });
 
-  $("#refresh").click(function(event) {
+
+
+      $("form#pizza-toppings").submit(function(event){
+          event.preventDefault();
+          $("input:checkbox[name=toppings]:checked").each(function(){
+            newPizza++;
+          });
+
+          $("input:checkbox[name=add-ons]:checked").each(function(){
+            newPizza++;
+          });
+
+          if (newPizza === 0) {
+            alert("You are not stressed");
+          } else if (newPizza > 0 && newPizza <= 5 ) {
+            alert("You're not that stressed, quit bitching");
+          } else if (newPizza > 5 && newPizza<= 9) {
+            alert("You're kinda stressed. Keep doing those coping methods");
+          }
+
+        });
+
+
+  $(".active").click(function(event) {
   location.reload(true);
 });
 });
