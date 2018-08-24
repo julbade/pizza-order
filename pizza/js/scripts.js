960ce18600e0d9a5ced76bea0fafe58dfe4c67ed
julius bade <julbade21@gmail.com>
2018-08-24 16:27:22 -0700

    will still fix some js scrpits

--- pizza/js/scripts.js
+++ pizza/js/scripts.js
@@ -43,24 +43,14 @@ Pizza.prototype.pizzaOrder = function() {
 
       $("form#pizza-toppings").submit(function(event){
           event.preventDefault();
-        var newPizza = 1
-        var toppingPrize = 1
-        var addOns =  2
-        $("input:checkbox[name=toppings]:checked").each(function(){
-            newPizza++;
-          });
-        $("input:checkbox[name=add-ons]:checked").each(function(){
-            newPizza++;
-          });
-        var pizzaPrice = new Pizza(toppingPrize + addOns);
+      var newPizza = 0
+      var toppingPrize = $("input:checkbox[name=toppings]:checked").val();
+      var addOns =  $("input:checkbox[name=add-ons]:checked").val();
+      var pizzaPrice = new Pizza(toppingPrize + addOns);
 
-          if (newPizza === 0) {
-            alert("Please check toppings!");
-          } else if (newPizza > 0 && newPizza <= 5 ) {
-            $("ul#price").append("<li><span class='orderPrice'>" + pizzaPrice.pizzaOrder() + "</span>");
-            $("#price").show();
-          } else if (newPizza > 5 && newPizza<= 9) {
-            $("ul#price").append("<li><span class='orderPrice'>" + pizzaPrice.pizzaOrder() + "</span>");
+
+        if ( newPizza === 0) {
+            alert("Please Check Toppings!");
           }
         });
 
