8ed6c597128e94c2fc154b5323d3a1559c5852e4
julius bade <julbade21@gmail.com>
2018-08-31 12:43:40 -0700

    added js script file to webpage

--- pizza/js/scripts.js
+++ pizza/js/scripts.js
@@ -1,17 +1,20 @@
 //back end
+var inputSize = {"small":10, "medium":12, "large":15};
+var inputCheese = {"less cheese":1, "normal cheese":4, "extra cheese":6};
+var inputSauce = {"bbq":2, "sauce":2, "alfredo":2};
+var inputMeat = {"chicken":3, "pepperoni":2, "sardines":4};
 
-
-function Pizza(name, cost) {
-  this.name = name;
-  this.cost = cost;
+function pizza(size, cheese, sauce, meat) {
+  this.size = size;
+  this.cheese = cheese;
+  this.sauce = sauce;
+  this.meat = meat;
 }
 
-Pizza.prototype.pizzaOrder = function() {
-  return this.cost;
+pizza.prototype.calculatePrice = function ()
+{
+  return inputSize[this.size] + inputCheese[this.cheese] + inputSauce[this.sauce] + inputMeat[this.meat]
 }
-
-
-
 
 // front end
 
@@ -21,40 +24,40 @@ Pizza.prototype.pizzaOrder = function() {
       $("#frontpage").hide();
       $("#nextpage").show();
       $(".details").show();
+      $(".menu").show();
   });
+
   $("#menu").click(function(event){
     event.preventDefault()
     $("#frontpage").hide();
     $("#nextpage").hide();
     $(".details").hide();
     $(".menu").show();
+
+  });
+
+  $("form#makePizza").submit(function(event){
+    event.preventDefault()
+  var pizzaSize = $("#size").val();
+  var pizzaCheese = $("#cheese").val();
+  var pizzaSauce = $("#sauce").val();
+  var pizzaMeat= $("#meat").val();
+  var newPizza = new pizza(pizzaSize, pizzaCheese, pizzaSauce, pizzaMeat);
+  $("#totalPrice").text(newPizza.calculatePrice() + " dollars");
+
 });
-
-
-
-        $("form#infos").submit(function(event)  {
-        event.preventDefault()
-        var firstnameInput = $("input#firstname").val();
-        var lastnameInput = $("input#lastname").val();
-        var addressInput = $("input#address").val();
+    $("form#infos").submit(function(event)  {
+    event.preventDefault()
+    var firstnameInput = $("input#firstname").val();
+    var lastnameInput = $("input#lastname").val();
+    var addressInput = $("input#address").val();
 
         $(".firstname").text(firstnameInput);
         $(".lastname").text(lastnameInput);
         $(".address").text(addressInput);
+
   });
 
-
-
-      $("form#pizza-toppings").submit(function(event){
-          event.preventDefault();
-      var newPizza = 0
-
-
-
-        if  (newPizza === 0) {
-            alert("Please Check Toppings!");
-          }
-        });
 
 
 
